c7444260ce830134b863b7418a665718a63145e1
AZBZ34 <alexbeazley18@outlook.com>
2021-11-22 20:00:46 +1100

    Led test working on mobile.

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.restful.home',
   appName: 'Restful Home',
-  webDir: 'www',
+  webDir: '../API/www',
   bundledWebRuntime: false
 };
 
--- client/src/app/tab2/tab2.page.ts
+++ client/src/app/tab2/tab2.page.ts
@@ -7,24 +7,7 @@ import { HTTP } from '@ionic-native/http/ngx';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  constructor(private http: HTTP) {}
 
-  async getData() {
-    try {
-      const url = 'http://192.168.1.100/led';
-      const params = {};
-      const headers = {};
+  constructor() {}
 
-      const response = await this.http.get(url, params, headers);
-
-      console.log(response.status);
-      console.log(JSON.parse(response.data)); // JSON data returned by server
-      console.log(response.headers);
-
-    } catch (error) {
-      console.error(error.status);
-      console.error(error.error); // Error message as string
-      console.error(error.headers);
-    }
-  }
 }
